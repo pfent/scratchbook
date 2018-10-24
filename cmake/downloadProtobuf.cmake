822259e4875cc416f24ab5d4d7d6e519b91b83d5
pfent <philipp@fent.de>
2018-10-24 19:07:39 +0200

    protobuf

---
+++ cmake/downloadProtobuf.cmake
@@ -0,0 +1,8 @@
+configure_and_build(protobuf)
+
+# Protobuf's gmock might clash with googletest
+set(protobuf_BUILD_TESTS OFF CACHE BOOL "do not build protobuf tests" FORCE)
+# Add the dependency directly to our build
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/protobuf/cmake
+        ${CMAKE_BINARY_DIR}/protobuf
+        EXCLUDE_FROM_ALL)
